04557be36c2cc37e8072b52ef0f0496af490eb8c
natsume <92217151+adam200134@users.noreply.github.com>
2023-09-03 01:17:19 +0800

    OK

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@ onMounted(() => {
 </script>
 <template>
   <div class="d-flex justify-content-center">
-    <iframe :src=videoUrl width="1440" height="810"
+    <iframe :src=videoUrl width="1280" height="720"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       allowfullscreen></iframe>
   </div>
